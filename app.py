b067a73711abcd3e7df2af2e03e88d0336ee8f68
Anthony <ndegwaanthony300@gmail.com>
2024-05-10 17:56:09 +0300

    updated the requirements file

--- app.py
+++ app.py
@@ -1,12 +1,36 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Data Analyst',
+        'location': 'Kenya',
+        'salary': 'ksh 120000' 
+    },
+    {
+        'id': 2,
+        'title': 'Backend Developer',
+        'location': 'USA',
+        'salary': '$1200k' 
+    },
+    {
+        'id': 3,
+        'title': 'Frontend Developer',
+        'location': 'Remote',
+        'salary': 'ksh 120000' 
+    }
+]
+
 @app.route("/")
 def show():
-    return "Hello Anthony"
+    return render_template("home.html", jobs=JOBS, company_name = "Ndegwa")
 
+@app.route("/api/jobs")
+def job_list():
+    return jsonify(JOBS)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True)
+    app.run(debug=True)
